f9ac22d6a8e39240c842537bce6351b9bedb27dd
hituji_code <k.m.hituji3340@gmail.com>
2024-02-01 12:40:09 +0900

    instagram

--- front-page.php
+++ front-page.php
@@ -128,6 +128,226 @@
       </div>
     </div>
   </div>
+
+  <div class="p-plan l-plan">
+    <div class="p-plan__inner l-inner">
+      <div class="p-plan__title">
+        <h2 class="c-section-title">
+          <span class="c-section-title__main">plan</span><span class="c-section-title__sub">プラン紹介</span>
+        </h2>
+      </div>
+      <div class="p-plan__content">
+        <ul class="p-list">
+          <li class="p-list__item">
+            <div class="p-list__item-img">
+              <img src="<?php echo esc_url(get_theme_file_uri('assets/images/plan/plan-img1.webp')); ?>" alt="" width="" height="" loading="lazy" />
+            </div>
+            <div class="p-list__item-body">
+              <h3 class="p-list__item-head"><span class="p-list__item-head-main">個々のアイデンティティを尊重し、<br>
+                  真摯に向き合うカウンセリング</span>
+                <span class="p-list__head-sub">（自己同一性、存在証明、同性愛など）</span>
+              </h3>
+              <p class="p-list__item-text">未来の幸せの形はさまざまです。私たちはそれを祝福致します。全ての幸せへの尊重を表現し、受け入れ、推奨する世界を一緒に創りたいと考えています。</p>
+              <div class="p-list__item-button">
+                <a class="c-button" href="#">more</a>
+              </div>
+            </div>
+          </li>
+          <li class="p-list__item">
+            <div class="p-list__item-img">
+              <img src="<?php echo esc_url(get_theme_file_uri('assets/images/plan/plan-img2.webp')); ?>" alt="" width="" height="" loading="lazy" />
+            </div>
+            <div class="p-list__item-body">
+              <h3 class="p-list__item-head"><span class="p-list__item-head-main">かけがえのない思い出になる
+                  <br>
+                  記念撮影</span>
+                <span class="p-list__head-sub">（ロケーションの提案、ヘアメイク、小道具貸出など）</span>
+              </h3>
+              <p class="p-list__item-text">同性、年齢、未婚や既婚問わず、ご自身の「今」を残す撮影をいたします。ロケーションの提案、ヘアメイクや小道具の貸出も承っております。一緒に思い出を作りましょう！
+              </p>
+              <div class="p-list__item-button">
+                <a class="c-button" href="#">more</a>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div class="p-plan__button">
+          <a class="c-button" href="#"><span>view all</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+  // caseの情報を配列に格納
+  $caseItems = [
+    [
+      'img' => 'assets/images/common/noimage@2x.webp',
+      'label' => '同性カップル',
+      'text' => '目黒様（仮）からのご相談',
+    ],
+    [
+      'img' => 'assets/images/common/noimage@2x.webp',
+      'label' => 'エイジングカップル',
+      'text' => '小野様（仮）からのご相談',
+    ],
+    [
+      'img' => 'assets/images/common/noimage@2x.webp',
+      'label' => '再婚カップル',
+      'text' => '稲葉様（仮）からのご相談',
+    ],
+  ] ?>
+  <div class="p-case l-case">
+    <div class="p-case__inner l-inner">
+      <div class="p-case__title">
+        <h2 class="c-section-title">
+          <span class="c-section-title__main">case</span><span class="c-section-title__sub">ご相談事例</span>
+        </h2>
+      </div>
+      <div class="p-case__content">
+        <ul class="p-cards">
+          <?php foreach ($caseItems as $item) : ?>
+            <li class="p-cards__item">
+              <a href="#" class="p-card">
+                <div class="p-card__img">
+                  <img src="<?php echo esc_url(get_theme_file_uri($item['img'])); ?>" alt="" width="" height="" loading="lazy" />
+                </div>
+                <div class="p-card__body">
+                  <p class="p-card__category"><?php echo esc_html($item['label']); ?></p>
+                  <p class="p-card__title"><?php echo esc_html($item['text']); ?></p>
+                </div>
+              </a>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+        <div class="p-case__button">
+          <a class="c-button" href="#"><span>view all</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+  // worksの情報を配列に格納
+  $worksItems = [
+    [
+      'img' => 'assets/images/works/wokrs1@2x.webp',
+      'label' => '十三詣り',
+      'text' => '丸山様（仮）の撮影',
+    ],
+    [
+      'img' => 'assets/images/works/wokrs2@2x.webp',
+      'label' => 'ソロ',
+      'text' => '鈴木様（仮）の撮影',
+    ],
+    [
+      'img' => 'assets/images/common/noimage@2x.webp',
+      'label' => 'エイジングカップル',
+      'text' => '稲葉様（仮）の撮影',
+    ],
+  ] ?>
+  <div class="p-works l-works">
+    <div class="p-works__inner l-inner">
+      <div class="p-works__title">
+        <h2 class="c-section-title">
+          <span class="c-section-title__main">works</span><span class="c-section-title__sub">撮影事例</span>
+        </h2>
+      </div>
+      <div class="p-works__content">
+        <ul class="p-cards">
+          <?php foreach ($worksItems as $item) : ?>
+            <li class="p-cards__item">
+              <a href="#" class="p-card">
+                <div class="p-card__img">
+                  <img src="<?php echo esc_url(get_theme_file_uri($item['img'])); ?>" alt="" width="" height="" loading="lazy" />
+                </div>
+                <div class="p-card__body">
+                  <p class="p-card__category"><?php echo esc_html($item['label']); ?></p>
+                  <p class="p-card__title"><?php echo esc_html($item['text']); ?></p>
+                </div>
+              </a>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+        <div class="p-works__button">
+          <a class="c-button" href="#"><span>view all</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+  // instaの情報を配列に格納 後でプラグインで実装
+  $instaItem = [
+    [
+      'img' => 'assets/images/top/instagram@2x.webp',
+    ],
+  ] ?>
+  <div class="p-instagram l-instagram">
+    <div class="p-instagram__inner l-inner">
+      <div class="p-instagram__title">
+        <h2 class="c-section-title">
+          <span class="c-section-title__main">instagram</span><span class="c-section-title__sub">インスタグラム</span>
+        </h2>
+      </div>
+      <div class="p-instagram__content">
+        <ul class="p-grid-list">
+          <?php for ($i = 0; $i < 10; $i++) : ?>
+            <li class="p-grid-list__item p-grid-item">
+              <div class="p-grid-item__img">
+                <img src="<?php echo esc_url(get_theme_file_uri($instaItem[0]['img'])); ?>" alt="" width="" height="" loading="lazy" />
+              </div>
+            </li>
+          <?php endfor; ?>
+        </ul>
+        <div class="p-instagram__button">
+          <a class="c-button--blank" href="#"><span>Instagram</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="p-contact l-contact">
+    <div class="p-contact__inner l-inner">
+      <div class="p-contact__title">
+        <h2 class="c-section-title">
+          <span class="c-section-title__main">contact</span><span class="c-section-title__sub">お問合せ</span>
+        </h2>
+      </div>
+      <div class="p-contact__content">
+        <div class="p-form">
+          <dl class="p-form__wrap">
+            <dt class="p-form__label"><label for="name">お名前<span>必須</span></label></dt>
+            <dd class="p-form__input p-form-input">
+              <input id="name" name="name" type="text" aria-required="true" required />
+            </dd>
+          </dl>
+          <dl class="p-form__wrap">
+            <dt class="p-form__label"><label for="email">メールアドレス<span>必須</span></label>
+            </dt>
+            <dd class="p-form__input p-form-input">
+              <input id="email" name="email" type="email" pattern=".+@.+\..+" />
+            </dd>
+          </dl>
+          <dl class="p-form__wrap">
+            <dt class="p-form__label"><label for="tel">電話番号<span>必須</span></label></dt>
+            <dd class="p-form__input p-form-input">
+              <input id="tel" name="tel" type="tel" />
+            </dd>
+          </dl>
+          <dl class="p-form__wrap">
+            <dt class="p-form__label"><label for="message">お問い合わせ内容<span>必須</span></label></dt>
+            <dd class="p-form__textarea p-form-textarea">
+              <textarea id="message" name="message"></textarea>
+            </dd>
+          </dl>
+          <div class="p-form__submit p-form-submit c-button">
+            <input type="submit" value="SEND">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- 共通 -->
 
 
@@ -143,82 +363,14 @@
       2回目の結婚だから…
       2.5次元推しの</p>
 
-    <ul class="p-list">
-      <li class="p-list__item">
-        <div class="p-list__item-img">
-          <img src="<?php echo esc_url(get_theme_file_uri('assets/images/plan/plan-img1.webp')); ?>" alt="" width="" height="" loading="lazy" />
-        </div>
-        <div class="p-list__item-body">
-          <h3 class="p-list__item-head"><span class="p-list__item-head-main">個々のアイデンティティを尊重し、<br>
-              真摯に向き合うカウンセリング</span>
-            <span class="p-list__head-sub">（自己同一性、存在証明、同性愛など）</span>
-          </h3>
-          <p class="p-list__item-text">未来の幸せの形はさまざまです。私たちはそれを祝福致します。全ての幸せへの尊重を表現し、受け入れ、推奨する世界を一緒に創りたいと考えています。</p>
-          <div class="p-list__item-button">
-            <a class="c-button" href="#">more</a>
-          </div>
-        </div>
-      </li>
-      <li class="p-list__item">
-        <div class="p-list__item-img">
-          <img src="<?php echo esc_url(get_theme_file_uri('assets/images/plan/plan-img2.webp')); ?>" alt="" width="" height="" loading="lazy" />
-        </div>
-        <div class="p-list__item-body">
-          <h3 class="p-list__item-head"><span class="p-list__item-head-main">かけがえのない思い出になる
-              <br>
-              記念撮影</span>
-            <span class="p-list__head-sub">（ロケーションの提案、ヘアメイク、小道具貸出など）</span>
-          </h3>
-          <p class="p-list__item-text">同性、年齢、未婚や既婚問わず、ご自身の「今」を残す撮影をいたします。ロケーションの提案、ヘアメイクや小道具の貸出も承っております。一緒に思い出を作りましょう！
-          </p>
-          <div class="p-list__item-button">
-            <a class="c-button" href="#">more</a>
-          </div>
-        </div>
-      </li>
-    </ul>
+
     <a class="c-button--blank" href="#" target="_blank"><span>Instagram</span></a>
 
-    <div class="p-form">
-      <dl class="p-form__wrap">
-        <dt class="p-form__label"><label for="name">お名前<span>必須</span></label></dt>
-        <dd class="p-form__input p-form-input">
-          <input id="name" name="name" type="text" aria-required="true" required />
-        </dd>
-      </dl>
-      <dl class="p-form__wrap">
-        <dt class="p-form__label"><label for="name">メールアドレス<span>必須</span></label>
-        </dt>
-        <dd class="p-form__input p-form-input">
-          <input id="email" name="email" type="email" pattern=".+@.+\..+" />
-        </dd>
-      </dl>
-      <dl class="p-form__wrap">
-        <dt class="p-form__label"><label for="name">電話番号<span>必須</span></label></dt>
-        <dd class="p-form__input p-form-input">
-          <input id="tel" name="tel" type="tel" />
-        </dd>
-      </dl>
-      <dl class="p-form__wrap">
-        <dt class="p-form__label"><label for="name">お問い合わせ内容<span>必須</span></label></dt>
-        <dd class="p-form__textarea p-form-textarea">
-          <textarea id="message" name="message"></textarea>
-        </dd>
-      </dl>
-      <div class="p-form__submit p-form-submit c-button">
-        <input type="submit" value="SEND">
-      </div>
-    </div>
 
 
 
-    <ul class="p-grid-list">
-      <li class="p-grid-list__item p-grid-item">
-        <div class="p-grid-item__img">
-          <img src="<?php echo esc_url(get_theme_file_uri('assets/images/top/instagram1.webp')); ?>" alt="" width="" height="" loading="lazy" />
-        </div>
-      </li>
-    </ul>
+
+
 
   </div>
   <div class="l-inner--large">
